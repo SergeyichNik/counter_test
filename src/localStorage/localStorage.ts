fba4bab436c39e8a37f1ef21f5b712d53ff0c61e
SergeichNik <tyunev.nik89@gmail.com>
2022-04-08 23:10:03 +0300

    bla bla

--- src/localStorage/localStorage.ts
+++ src/localStorage/localStorage.ts
@@ -1,6 +1,16 @@
 import {IGlobalState} from "../bll/state";
 import {initialState} from "../bll/counterReducer";
 
+const state: IGlobalState = {
+    counter: {
+        value: 0,
+        minValue: 0,
+        maxValue: 5,
+        countStep: 1,
+        isSetMode: false,
+        isError: false,
+    }
+}
 
 export const saveToLS = (state: IGlobalState) => {
     const stateTo = JSON.stringify(state)
@@ -12,7 +22,7 @@ export const loadFromLS = () => {
     if (stateFrom) {
         return JSON.parse(stateFrom)
     } else {
-        return initialState
+        return state
     }
 
 }
